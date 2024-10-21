28dffad43dbed50c5ee8ecacd7e199dedef64af6
Guanghong Zheng <gz424@ic.ac.uk>
2024-10-21 16:15:46 +0100

    Update

--- src/app.py
+++ src/app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request
 
+
 app = Flask(__name__)
 
 
@@ -13,3 +14,11 @@ def submit():
 @app.route("/")
 def hello_world():
     return render_template("index.html")
+
+
+@app.route("/get", methods=["GET"])
+def process_query(query):
+    if query=="dinosaurs":
+        return "Dinosaurs ruled the Earth 200 million years ago"
+    else:
+        return "Unknown"
