ccbfbf0413a6aef1af47e7fa4a4509b02161383f
Noah <1348060613@qq.com>
2024-11-05 15:08:50 +0000

    Lab5 V2.0

--- src/app.py
+++ src/app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request
 import requests as re
+import os
 
 
 app = Flask(__name__)
@@ -27,16 +28,40 @@ def process_query(q):
 
 @app.route("/submitgetgithubusername", methods=["POST"])
 def submitgetgithubusername():
+    data = []
     input_name = request.form.get("name")
     response = re.get("https://api.github.com/users/{}/repos".format(input_name))
+    url = "https://api.github.com/repos/{}/{}/commits"
     if response.status_code == 200:
-        repos = response.json()  # data returned is a list of ‘repository’ entities
-        for repo in repos:
-            print(repo["full_name"])
-    return render_template("github_username.html", name=input_name)
-
+        for repo in response.json():
+            temp = {}
+            temp["name"] = repo['name']
+            response1 = re.get(url.format(input_name,repo['name']))
+            if response1.status_code == 200:
+                try:
+                    commit = response1.json()[0]
+                    temp['SHA'] = commit['sha']
+                    temp['Author'] = commit['commit']['author']['name']
+                    temp['Date'] = commit['commit']['author']['date']
+                    temp['Message'] = commit['commit']['message']
+                except:
+                    continue
+                data.append(temp)
+    return render_template("github_username.html", name=input_name, data=data)
 
 
 @app.route("/get_github_username", methods=["GET"])
 def get_github_username():
     return render_template("get_github_username.html")
+
+
+@app.route("/get_currency_exchange", methods=["GET", "POST"])
+def get_currency_exchange():
+    data = {}
+    target = ["date","rates"]
+    token = "f4f934d860f2881959b0bd1b9ff5efd4"
+    url = "http://data.fixer.io/api/latest?access_key={}"
+    response = re.get(url.format(token))
+    if response.status_code == 200:
+        data = {k: response.json()[k] for k in target if k in response.json()}
+    return render_template("get_currency_exchange.html", data=data)
